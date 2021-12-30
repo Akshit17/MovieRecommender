110810e161ab740cb3c5adc7091d8f6f29cc057d
Akshit17 <akshit.chodhary@gmail.com>
2021-12-30 16:36:56 +0530

    Updated dataset

--- data_finalize/data_finalize.py
+++ data_finalize/data_finalize.py
@@ -11,6 +11,46 @@ def convert(obj):                         #function to get columns like 'genre' 
         pass
     return L
 
+def convert_2(obj):
+    L = []
+    try:
+        for i in ast.literal_eval(obj):
+            L.append(i['character'])
+    except:
+        pass
+    return L
+
+def convert_3(obj):
+    L = []
+    # print(type(obj))
+    if type(obj) == float:          # null values are listed in data in this format
+        obj = None
+    else:
+        # print(type(obj))
+        obj = obj.split('|')
+        # print(obj)
+    return obj
+
+def first_four(obj):
+    if type(obj) == float:          # null values are listed in data in this format
+        obj = None
+    else:
+        # print(type(obj))
+        obj = obj[0:4]
+        # print(obj)
+    return obj
+
+def last_four(obj):
+    if type(obj) == float:          # null values are listed in data in this format
+        obj = None
+    else:
+        print(type(obj))
+        print(obj)
+        obj = obj[-4:]
+        # print(obj)
+    return obj
+
+
 
 credits = pd.read_csv("./kaggle_moviedataset/credits.csv")
 keywords = pd.read_csv("./kaggle_moviedataset/keywords.csv")
@@ -19,6 +59,8 @@ links = pd.read_csv("./kaggle_moviedataset/links.csv")
 ratings_small = pd.read_csv("./kaggle_moviedataset/ratings_small.csv")
 ratings = pd.read_csv("./kaggle_moviedataset/ratings.csv")
 movies_metadata = pd.read_csv("./kaggle_moviedataset/movies_metadata.csv")
+
+tmdb_movies_metadata = pd.read_csv("./kaggle_moviedataset/tmdb_movies_data.csv")
 
 
 ml_25_genome_scores = pd.read_csv("./ml-25m/genome-scores.csv")
@@ -42,12 +84,13 @@ print(movies_metadata.iloc[0].genres)
 # Need to convert "[{'id': 16, 'name': 'Animation'}, {'id': 35, 'name': 'Comedy'}, {'id': 10751, 'name': 'Family'}]" 
 #  Into [ 'Animation', 'Comedy', 'Family']
 
-movies_metadata.shape
-movies_metadata.duplicated().sum()      #gave 13
+print(movies_metadata.shape)
+print(movies_metadata.duplicated().sum())      #gave 13
 
-
+print(movies_metadata.head(10))
 
 movies_metadata = movies_metadata.sort_values(by="id")
+movies_metadata['id'] = movies_metadata['id'].astype(str)
 print(type(movies_metadata['id'][0]))
 #to merge the keywords.csv and movie_metadata.csv the id datatype should be same
 
@@ -99,13 +142,51 @@ movies_metadata = movies_metadata.drop_duplicates(subset=["id"] ,keep="first")
 print(movies_metadata[movies_metadata["id"].duplicated()])
 # movies_metadata["id"].nunique()   45432 unique which is equal to shp so all gud
 
-movies_metadata = movies_metadata[["title","genres","imdb_id","id", "original_language",  "overview", "popularity" ,"poster_path", "production_companies", "production_countries" , "release_date", "revenue", "spoken_languages", "vote_average" , "vote_count" , "keywords" , "cast", "crew"]]
+movies_metadata = movies_metadata[["title","genres","imdb_id","id", "overview",  "cast", "keywords" ,"poster_path", "original_language", "release_date"]]
 
 movies_metadata = movies_metadata.rename(columns={"id":"tmdb_id"})
 
+# print(type(movies_metadata['cast']))
+# print(ast.literal_eval(movies_metadata['cast'][0]))
+
+movies_metadata['characters'] = movies_metadata['cast'].apply(convert_2)
+movies_metadata['cast'] = movies_metadata['cast'].apply(convert)
+
+movies_metadata = movies_metadata.rename(columns={"release_date" : "release_year"})
+movies_metadata['release_year'] = movies_metadata['release_year'].apply(first_four)
+
+movies_metadata = movies_metadata.sort_values(by="release_year",ascending=False)
+
+#removing rows with empty values and empty lists in overview and cast
+movies_metadata = movies_metadata.dropna()
+movies_metadata = movies_metadata[movies_metadata['overview'] != '[]']
+movies_metadata = movies_metadata[movies_metadata['cast'] != '[]']
+# movies_metadata = movies_metadata[movies_metadata['overview'].map(lambda d: len(d)) > 0]
+
 
 movies_metadata.to_csv('movies_metadata.csv', index = False)
-# delete movies with no poster path and no genres
+
+
+
+tmdb_movies_metadata = tmdb_movies_metadata.rename(columns={"id":"tmdb_id" , "original_title" : "title" })
+tmdb_movies_metadata = tmdb_movies_metadata[["title","genres","imdb_id","tmdb_id", "overview",  "cast", "keywords" , "release_year"]]
+tmdb_movies_metadata['cast'] = tmdb_movies_metadata['cast'].apply(convert_3)
+tmdb_movies_metadata['keywords'] = tmdb_movies_metadata['keywords'].apply(convert_3)
+tmdb_movies_metadata['genres'] = tmdb_movies_metadata['genres'].apply(convert_3)
+
+# tmdb_movies_metadata['release_year'] = tmdb_movies_metadata['release_year'].apply(last_four)
+
+tmdb_movies_metadata.to_csv('tmdb_movies_metadata.csv', index = False)
+
+
+
+
+
+
+
+
+
+
 
 #TO-DO: From ml-25m dataset replace movieid with corresponding tmbd/imbd id
 
@@ -138,6 +219,3 @@ ml_25_movies.to_csv('ml_25_movies.csv', index = False)
 
 # ml_25_movies.csv is missing few columns like 'overview' , 'cast', 'crew' when compared with movies_metadata.csv 
 
-import imdb
-
-moviesDB = imdb.IMDb()
