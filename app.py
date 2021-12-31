11edc06c69717cb6408bde2632e5b4a8b90d6c8f
Akshit17 <akshit.chodhary@gmail.com>
2022-01-01 02:03:20 +0530

    Settting up document

---
+++ app.py
@@ -0,0 +1,32 @@
+import csv
+from jina import Document, DocumentArray, DocumentArrayMemmap
+
+
+# da = DocumentArray.load_csv("./movies_metadata.csv")
+#UnicodeDecodeError: 'charmap' codec can't decode byte 0x9d in position 455: character maps to <undefined>
+
+
+# da = DocumentArray.load_csv("./movies_metadata.csv", encoding="utf8")
+#gives error with encoding .  No attribute named 'encoding' either to specify encoding or to use utf8
+
+
+
+with open("movies_metadata.csv", encoding="utf-8") as file:
+    #setup csv reader
+    reader = csv.DictReader(file)
+    #setup list to hold dictionaries
+    movies = []
+    #loop through each row in the csv
+    for row in reader:
+        # print(row.keys())
+        #append each row to the list
+        movies.append(row['title'])
+    print(movies)
+    da = DocumentArray.from_csv(file)
+
+
+# print(da[])
+print(da[2].tags)
+
+
+print(da[1].text)
