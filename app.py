00d98e6218a9c1712281921948b44a99762af88b
Akshit17 <akshit.chodhary@gmail.com>
2022-01-08 01:31:47 +0530

    Trying out Encoders and Indexers

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import ast
 import csv
 from jina import Document, DocumentArray, DocumentArrayMemmap
+from jina import Flow
 
 
 # da = DocumentArray.load_csv("./movies_metadata.csv")
@@ -26,6 +27,8 @@ with open("movies_metadata.csv", encoding="utf-8") as file:
         movies.append(row['overview'])
         da = Document(text=row['overview']) #tags=ast.literal_eval(row['cast'])
         da.tags['genres'] = ast.literal_eval(row['genres'])
+        da.tags['title'] = row['title']
+        
         # da.tags['cast'] = ast.literal_eval(row['cast'])
         # da.tags['keywords'] = ast.literal_eval(row['keywords'])
         docs.append(da)
@@ -35,6 +38,8 @@ with open("movies_metadata.csv", encoding="utf-8") as file:
     # da.plot()
 
 # print(docs[0].json())
+# for d in docs:
+#     d.plot()
 
 print(type(docs.get_vocabulary()))
 # docs.plot_image_sprites(output=None, canvas_size=512, min_size=16, channel_axis=- 1)
@@ -47,3 +52,48 @@ print(type(docs.get_vocabulary()))
 
 
 # print(da[1].text)
+
+if __name__ == "__main__":
+    model = "sentence-transformers/paraphrase-distilroberta-base-v1"
+
+    flow = (
+        Flow(install_requirements = True)
+        .add(
+            name="Text_Encoder",
+            uses="jinahub://TransformerTorchEncoder",
+            uses_with={"pretrained_model_name_or_path": model},
+            install_requirements = True
+        )
+        .add(
+            name="Text_Indexer",
+            uses='jinahub://SimpleIndexer',
+            uses_metas={'workspace': '/indexing/tmp_folder'},
+            install_requirements = True
+        )
+    )
+
+    print(flow)
+
+    # with flow:
+    #     flow.index(
+    #         inputs=docs,
+    #   )
+
+    with flow:
+        flow.index(
+        inputs=docs,
+            docs = docs,
+            parameters = {'name' : 'something', 'xyz' : 'fsdfsdfsa'}
+    )
+
+    query = Document(text = input('Query song : '))
+    with flow:
+        response = flow.search(inputs = query, return_results = True)
+
+    matches = response[0].docs[0].matches
+        
+    for ind, i in enumerate(matches):
+        print(f' Movie Title :  {i.tags["title"]} '.center(60,'='))
+        print()
+        print(i.text)
+        print()
